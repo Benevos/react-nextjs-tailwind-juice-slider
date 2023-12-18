d4ef9b23d6340748d177503e8594306ab791fc89
Benevos <b4dshot_assassin@hotmail.com>
2023-12-17 19:58:29 -0600

    repository remade

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,96 @@
-import Image from 'next/image'
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable @next/next/no-img-element */
+"use client";
 
-export default function Home() {
+import React, { useEffect, useState } from "react";
+
+import Carrousel from "@/components/Carrousel";
+import Header from "@/components/Header";
+
+export default function Home() 
+{
+  const products = [
+    {name: 'Orange', image: '/juice_1.png', color: '#ffb300', flavorNameColor: '#00b545', fruitTop: '/fruit_top_1.png', fruitBottom: '/fruit_bottom_1.png'},
+    {name: 'Watermelon', image: '/juice_2.png', color: '#0f825b', flavorNameColor: '#ffffff', fruitTop: '/fruit_top_2.png', fruitBottom: '/fruit_bottom_2.png'},
+    {name: 'Pineapple', image: '/juice_3.png', color: '#d9142b', flavorNameColor: '#ffb300', fruitTop: '/fruit_top_3.png', fruitBottom: '/fruit_bottom_3.png'}
+  ]
+ 
+  const [flavors, setFlavors] = useState(products);
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [lastAnimationStartTime, setLastAnimationStartTime] = useState(0);
+  const [backgroundColor, setBackgroundColor] = useState('#ffb300');
+
+ 
+  const changeSlidePropierties = (slides = [], activeAnimationClass = '') =>
+  {
+    const bottles = products.length - 1;
+
+    slides[currentSlide].classList.remove(activeAnimationClass);
+    slides[currentSlide].classList.remove('slide-initial');
+
+    slides[currentSlide].classList.add('slide-hidden');
+
+    setTimeout(() =>
+    {
+      slides[currentSlide].classList.remove('slide-hidden');
+      slides[currentSlide].classList.add('slide-stand');
+    }, 1500)
+
+    if(bottles <= currentSlide)
+    {
+      slides[0].classList.remove('slide-stand');
+      slides[0].classList.add(activeAnimationClass);
+      setCurrentSlide(0);
+      return;
+    }
+    
+    slides[currentSlide+1].classList.remove('slide-stand');
+    slides[currentSlide+1].classList.add(activeAnimationClass);
+    setCurrentSlide(currentSlide+1);
+    
+  }
+
+  const handleNextSlide = (e) =>
+  {
+    if(e) if(e.target.id === 'main-image') return;
+    
+    const currentAnimationStartTime = performance.now();
+    const timeDifferenceMs = currentAnimationStartTime - lastAnimationStartTime;
+
+    if(!(timeDifferenceMs >= 1600)) return;
+
+    setLastAnimationStartTime(currentAnimationStartTime);
+
+    const bottleSlides = document.querySelectorAll('div[name="bottle-slide"]');
+    const titleSlides = document.querySelectorAll('div[name="title-slide"]');
+    const fruitTopSlides = document.querySelectorAll('div[name="fruit-top-slide"]');
+    const fruitBottomSlides = document.querySelectorAll('div[name="fruit-bottom-slide"]');
+
+    changeSlidePropierties(bottleSlides, 'bottle-active');
+    changeSlidePropierties(titleSlides, 'title-active');
+    changeSlidePropierties(fruitTopSlides, 'fruit-active');
+    changeSlidePropierties(fruitBottomSlides, 'fruit-active');
+  }
+
+  useEffect(() =>
+  {
+    setBackgroundColor(flavors[currentSlide].color);
+
+    const interval = setInterval(handleNextSlide, 4000);
+
+    return () => {
+      clearInterval(interval); // Limpieza al desmontar el componente
+    };
+
+  }, [currentSlide]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main className="h-full flex flex-col">
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <Header backgroundColor={backgroundColor} textColor={flavors[currentSlide].flavorNameColor}/>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <Carrousel flavors={flavors} backgroundColor={backgroundColor} currentIndex={currentSlide} onClick={handleNextSlide}/>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   )
 }
